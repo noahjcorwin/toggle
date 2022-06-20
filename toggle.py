20ffe028ce93924f3cabcb48e6683e9c6a0547bb
Noah Corwin <42302279+noahjcorwin@users.noreply.github.com>
2022-06-19 22:06:55 -0700

    Update toggle.py

--- toggle.py
+++ toggle.py
@@ -5,16 +5,10 @@ from flask import Flask
 from flask_httpauth import HTTPTokenAuth
 
 cwd = os.getcwd()
-
-black_list_file = f"{cwd}/blacklist.txt"
 token_file = f"{cwd}/tokens.txt"
 
 with open(token_file, 'r') as f:
     tokens= json.loads(f.read())
-
-black_list = []
-with open(black_list_file) as f:
-    black_list = f.read().splitlines()
 
 app = Flask(__name__)
 auth = HTTPTokenAuth(scheme='Bearer')
@@ -27,31 +21,29 @@ def verify_token(token):
 @app.route('/toggleon')
 @auth.login_required
 def toggleon():
-    for item in black_list:
-        command = ['/usr/local/bin/pihole','--regex',item]
-        proc = subprocess.Popen(command,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-        )
-        stdout, stderr = proc.communicate()
-        return_code = proc.returncode
-        std_out = stdout.decode("utf-8")
-        std_err = stderr.decode("utf-8")
+    command = ['/usr/bin/sudo','/usr/bin/python3','/opt/toggle/on.py']
+    proc = subprocess.Popen(command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    return_code = proc.returncode
+    std_out = stdout.decode("utf-8")
+    std_err = stderr.decode("utf-8")
     return 'BLOCKING ON'
 
 @app.route('/toggleoff')
 @auth.login_required
 def toggleoff():
-    for item in black_list:
-        command = ['/usr/local/bin/pihole','--regex',item]
-        proc = subprocess.Popen(command,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-        )
-        stdout, stderr = proc.communicate()
-        return_code = proc.returncode
-        std_out = stdout.decode("utf-8")
-        std_err = stderr.decode("utf-8")
+    command = ['/usr/bin/sudo','/usr/bin/python3','/opt/toggle/off.py']
+    proc = subprocess.Popen(command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    return_code = proc.returncode
+    std_out = stdout.decode("utf-8")
+    std_err = stderr.decode("utf-8")
     return 'BLOCKING OFF'
 
 if __name__ == '__main__':
